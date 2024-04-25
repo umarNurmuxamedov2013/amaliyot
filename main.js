8b0adf69453ba8dc143a89f3fa558b9369ac72ea
umarNurmuxamedov2013 <nurmuxamedovumar@gmail.com>
2024-04-25 10:55:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const lorem = document.querySelector('p')
+const body = document.querySelector('body')
+
+
+let color = prompt("enter text color")
+let background = prompt("enter text background")
+
+lorem.style.color = color ;
+body.style.backgroundColor = background ;
